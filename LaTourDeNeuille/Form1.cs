d10339ef2b5096954764b983c3cdc526d991fa8f
girardl2 <girardl2@s2.rpn.ch>
2025-09-02 10:48:31 +0200

    init du projet

--- LaTourDeNeuille/Form1.cs
+++ LaTourDeNeuille/Form1.cs
@@ -16,5 +16,10 @@ namespace LaTourDeNeuille
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
